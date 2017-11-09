415bd0652ac3a319427d738e0ddc470d8b6818f6
oscaryald <oscaryald@gmail.com>
2017-11-10 01:11:24 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+
+// npm install gulp --save-dev
+
+var gulp = require('gulp'); // Require gulp
+
+// Sass dependencies
+var sass = require('gulp-sass'); // Compile Sass into CSS
+// var sourcemaps = require('gulp-sourcemaps');
+// var autoprefixer = require('gulp-autoprefixer');
+
+// var imagemin = require('gulp-imagemin'); // Minify images
+
+// // Other dependencies
+// var size = require('gulp-size'); // Get the size of the project
+var browserSync = require('browser-sync').create(); // Reload the browser on file changes
+
+// var gulpSequence = require('gulp-sequence');
+// var newer = require('gulp-newer');
+
+// var clean = require('gulp-clean');
+
+// // Tasks -------------------------------------------------------------------- >
+
+// // Task to compile Sass file into CSS, and minify CSS into build directory
+gulp.task('styles', function() {
+  gulp.src('./source/sass/style.scss')
+    // .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(autoprefixer())
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./source/css'))
+    // .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.reload({
+      stream: true,
+    }));
+});
+
+// // Task to minify images into build
+// gulp.task('images', function() {
+//   gulp.src('./source/img/*')
+//   .pipe(imagemin({
+//     progressive: true,
+//   }))
+//   .pipe(gulp.dest('./build/img'));
+// });
+
+// // Task to get the size of the app project
+// gulp.task('size', function() {
+//   gulp.src('./source/**')
+//     .pipe(size({
+//     showFiles: true,
+//   }));
+// });
+
+// // Task to get the size of the build project
+// gulp.task('build-size', function() {
+//   gulp.src('./build/**')
+//   .pipe(size({
+//     showFiles: true,
+//   }));
+// });
+
+// gulp.task('assets', function(){
+//    return gulp.src('source/**')
+//     .pipe(newer('build'))
+//     // .on('data', function(file){
+//     //   console.log(file)
+//     // })
+//     .pipe(gulp.dest('build'));
+// });
+
+// gulp.task('cleanAll', function(){
+//    return gulp.src('build/*', {read: false})
+//         .pipe(clean()); 
+// });
+
+// gulp.task('build', gulpSequence('cleanAll', ['assets', 'styles', 'images', 'size']));
+
+// // Serve application
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './sourse',
+    },
+  });
+});
+
+
+// // Run all Gulp tasks and serve application
+gulp.task('dev', ['serve'], function() {
+  gulp.watch('source/sass/**/*.scss', ['styles']);
+  gulp.watch('source/*.html', browserSync.reload);
+  gulp.watch('source/js/*.js', browserSync.reload);
+  gulp.watch('source/**/*.*', ['assets']);
+});
---
+++ sourse/js/main.js
@@ -0,0 +1,306 @@
+$(document).ready(function(){
+
+	function Post(param){
+		this.countComments = param.countComments || 1;
+		this.loadMoreComments = param.loadMoreComments || 1;
+		this.items = [];
+	}
+
+	Post.prototype = {
+
+		constructor: Post,
+
+		init : function(){
+			this.getPost();
+			this.addEventHandler();
+		},
+
+		loadMore: function(arr){
+			if(this.countComments + this.loadMoreComments > this.countComments){
+				this.countComments += this.loadMoreComments
+			}
+			this.getPost()
+			return 
+		},
+
+		addCommentsTemplate : function(comment){
+			var that = this;
+			var commentsTemplate = $('#comments-template').html()
+			comment.created_at = that.dateReviver(comment.created_at)
+			for(var i = 0; i < comment.children.length; i++){
+				comment.children[i].created_at = that.dateReviver(comment.children[i].created_at)
+			}
+			$( ".comments-list" ).append(Mustache.render(commentsTemplate, comment));
+		},
+
+		addEventHandler: function(){
+			var that = this,
+				textarea,
+				commentId;
+
+			this.addClassToSend();	
+
+			$('.load-more').on('click', function(){
+				that.loadMore(that.items);
+				return false
+			});
+
+			$('.comments-content').on('click','.btn[data-class="edit"]', function(){
+
+				textarea = $(this).parents('li').find('.comment-content');
+				commentId = $(this).parents('li').attr('id');
+
+				if(textarea.val() == ''){
+					textarea.addClass('error');
+					return false
+				}else{
+					textarea.removeClass('error');
+				} 
+
+				that.editComment.apply($(this), [textarea.val(), commentId]);
+				$(this).parents('li').find('.comment-content').val('');
+				return false
+			});
+
+			$('.comments-content').on('click','.delete', function(){
+				commentId = $(this).parents('li').attr('id');
+				that.deleteComment.apply($(this), [commentId]);
+				return false
+			})
+
+
+			$('.comments-content').on('click','.btn[data-class="reply"]', function(){
+				textarea = $(this).parents('li').find('.comment-content');
+
+				if(textarea.val() == ''){
+					textarea.addClass('error');
+					return false
+				}else{
+					textarea.removeClass('error');
+				} 
+
+				that.replayComment.apply($(this), [textarea.val()]);
+				textarea.val('');
+				return false
+			})
+
+			$('.comments-block').on('click','.add', function(){
+				textarea = $(this).parents('.message-field').find('.comment-content');
+
+				if(textarea.val() == ''){
+					textarea.addClass('error');
+					return false
+				}else{
+					textarea.removeClass('error');
+				} 
+
+				that.addNewComment.apply($(this), [textarea.val()]);
+				textarea.val('');
+				return false
+			});
+
+			$('.comments-block').on('click','.cancel', function(){
+				$(this).parents('.message-field__textarea').slideUp(300);
+				return false
+			})
+
+
+		},
+
+		addClassToSend : function(){
+			$('.comments-content').on('click','.edit, .reply', function(){
+				var btn = $(this).parents('.message-field').find('.message-field__textarea .btn')
+				$(this).parents('.message-field').find('.message-field__textarea').slideDown(300);
+				btn.attr('data-class', '');
+				btn.attr('data-class', $(this).attr('class'));
+				return false
+			})
+		},
+
+		dateReviver : function(value){
+			var ms = Date.parse(value),
+				    date = new Date(ms),
+				    year = date.getFullYear(),
+				    month = date.getMonth()+1,
+				    day = date.getDate(),
+				    minutes = ( date.getMinutes()<=9 ) ? '0' + date.getMinutes() : date.getMinutes(),
+				    hours = date.getHours();
+				value = year + '-' + month + '-' + day + ' at ' + hours + ':' + minutes;
+			    return value;
+		},
+
+		getPost : function(param){
+			var that = this;
+
+			function load(){
+				$('.comments-list').html('');
+				that.items = [];
+
+				$.ajax({
+				    url: 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/',
+				    type: 'GET',
+				    dateType: 'json',
+				    success: function(data){
+					    $.each( data, function( key, val ) {
+						    if (key < that.countComments) {
+						    	that.addCommentsTemplate(val);
+							}
+					    });
+				    }
+				})	
+
+			};
+
+			load();
+		},
+
+		addNewComment : function(newComment){
+			var id = $(this).parents('li').attr('id');
+			var targetComment = $(this);
+			var comment = {
+				content : newComment,
+				id: id || null
+			}
+
+			$.ajax({
+				type: "POST",
+				url : 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/',
+				data:comment,
+				success: function(data){
+					var commentsTemplate = $('#comments-template').html();
+					data.created_at = Post.prototype.dateReviver.apply(Post, [data.created_at])
+					$( ".comments-list" ).prepend(Mustache.render(commentsTemplate, data));
+					console.log(data.content)
+				}
+
+			})
+			return false
+		},
+
+		replayComment : function(newComment){
+			var id = $(this).parents('li').attr('id');
+			var targetUL  = $(this).parents('.comment').find('.children-list');
+			var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+			var comment = {
+				children : [
+					{
+						content : newComment,
+						id: id
+					}
+				]
+			}
+
+			$.ajax({
+				type: "PUT",
+				url : url + id,
+				data:comment,
+				success: function(data){
+					console.log(comment.children[0].content)
+					data.created_at = Post.prototype.dateReviver.apply(Post, [data.created_at])
+					var result = '<li class="flex-wrap clearfix">'+
+		              '<div class="avatar"><img src="'+data.author.avatar+'" alt=""></div>'+
+		              '<div class="message-field"><div class="author-name">'+data.author.name+'</div>'+
+		              '<div class="date"><i class="fa fa-clock-o"></i>'+data.created_at+'</div>'+
+		              '<p>'+comment.children[0].content+'</p></li>';
+
+					targetUL.append(result)
+				}
+			})
+			return false
+		},
+
+		deleteComment : function(id){
+				var elementId = $('#'+id) 
+				var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+			    $.ajax({
+			         type: "DELETE",
+			         url: url + id,
+			         dataType: "json",
+			         success: function (data, status, jqXHR) {
+			            elementId.remove()
+			         }
+			     });
+			    return false
+		},
+
+		editComment : function(newComment, id){
+				var elementId = $('#'+id);
+				var comment = {
+					content : newComment
+				} 
+				var url = 'http://frontend-test.pingbull.com/pages/oscaryld@gmail.com/comments/';
+
+			    $.ajax({
+			         type: "PUT",
+			         url: url + id,
+			         dataType: "json",
+			         data:comment,
+			         success: function (data, status, jqXHR) {
+			         	elementId.find('p').html(newComment)
+			         }
+			     });
+			    return false
+		},
+	}
+
+	var post = new Post({
+		countComments:3,
+		loadMoreComments: 1
+	});
+
+	post.init();
+
+});
+
+
+window.onload = function(){
+	
+	function DataTable(param){
+
+		this.table = document.getElementById(param.block)
+
+	}
+
+	DataTable.prototype = {
+
+		constructor: DataTable,
+
+		init: function(){
+			this.load()
+		},
+
+		load : function(){
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', '/js/MOCK_DATA.json');
+			xhr.onload = function() {
+			    if (xhr.status === 200) {
+
+			    	var data = JSON.parse(xhr.responseText)
+			    	// console.log(data)
+
+			    	data.forEach(function(el, i){
+			    		console.log(el['First Name'])
+			    		// console.log(el['ID'])
+			    	})
+			        
+			        // console.log(xhr.responseText)
+			    }
+			    else {
+			       console.log('Request failed.  Returned status of ' + xhr.status);
+			    }
+			};
+			xhr.send();
+
+		}
+
+	}
+
+	var dataTable = new DataTable({
+		block: 'data-table'
+	});
+	dataTable.init()
+
+}
+
+
