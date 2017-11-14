efa829f6a20f3ae2009c9a588fa58dd0142ba90f
Aleksandr.Trokhimenk <aleksandr.trokhimenko@devellar.com>
2017-11-14 16:08:36 +0200

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 
 // npm install gulp --save-dev
-
 var gulp = require('gulp'); // Require gulp
 
 // Sass dependencies
@@ -8,19 +7,14 @@ var sass = require('gulp-sass'); // Compile Sass into CSS
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
-// var imagemin = require('gulp-imagemin'); // Minify images
-
 // // Other dependencies
-// var size = require('gulp-size'); // Get the size of the project
 var browserSync = require('browser-sync').create(); // Reload the browser on file changes
 
 var gulpSequence = require('gulp-sequence');
 var newer = require('gulp-newer');
-
 var clean = require('gulp-clean');
 
 // // Tasks -------------------------------------------------------------------- >
-
 // // Task to compile Sass file into CSS, and minify CSS into build directory
 gulp.task('styles', function() {
   gulp.src('./sourse/sass/styles.scss')
@@ -29,44 +23,15 @@ gulp.task('styles', function() {
     .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./sourse/css'))
-    // .pipe(minifyCSS())
     .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.reload({
       stream: true,
     }));
 });
 
-// // Task to minify images into build
-// gulp.task('images', function() {
-//   gulp.src('./source/img/*')
-//   .pipe(imagemin({
-//     progressive: true,
-//   }))
-//   .pipe(gulp.dest('./build/img'));
-// });
-
-// // Task to get the size of the app project
-// gulp.task('size', function() {
-//   gulp.src('./source/**')
-//     .pipe(size({
-//     showFiles: true,
-//   }));
-// });
-
-// // Task to get the size of the build project
-// gulp.task('build-size', function() {
-//   gulp.src('./build/**')
-//   .pipe(size({
-//     showFiles: true,
-//   }));
-// });
-
 gulp.task('assets', function(){
    return gulp.src('sourse/**')
     .pipe(newer('build'))
-    // .on('data', function(file){
-    //   console.log(file)
-    // })
     .pipe(gulp.dest('build'));
 });
 
@@ -85,7 +50,6 @@ gulp.task('serve', function() {
     },
   });
 });
-
 
 // // Run all Gulp tasks and serve application
 gulp.task('dev', ['build', 'serve'], function() {
--- sourse/js/content.js
+++ sourse/js/content.js
@@ -1,55 +1,258 @@
-/*
-	#HTML/CSS/JS TEST SPECIFICATION AND REQUIREMENTS
 
-	##Page overals
-	- Liquid layout
-	- max content wrapper width: 1280px, min widht: 800
-	- content centered on the page
-	- right column width: 30% of content wrapper
-	- left column and right column padding 10px
-	- - - either column can lead the height of the page
-	- page main title - embed font -> HelveticaInserat LT
-	- logo element must be fixed at all time at the left border of the page
-	- all of your JS code MUST be into the "content.js" file
-	- do not use any ready scripts and libraries
+window.onload = function(){
 
-	##Dynamics
-	- main navigation, drop down menu based on JS - don't use ready scripts
-	- right column dynamic boxes:
-	- - - click to open, click to close
-	- - - two boxes must not be open in the same time 
+	function removeClases(block, sublingElement, className, callback){
+		for (var i = 0; i < block.getElementsByTagName(sublingElement).length; i++){
+		    block.getElementsByTagName(sublingElement)[i].classList.remove(className)
+		    if(typeof callback === 'function'){
+		    	callback(block.getElementsByTagName(sublingElement)[i])
+		    }
+		}
+	}
+	
+	function DataTable(param){
+		this.block = param.block;
+		this.blockTable = document.getElementById(param.block);
+		this.table = document.createElement('table');
+	}
 
-	##Cross-browser
-	- 10+
-	- FF
-	- Chrome
+	DataTable.prototype = {
 
-	##Language menu
-	- hover makes flag opaque
-	- selected flag is opaque
+		constructor: DataTable,
 
-	##Misc
-	- font sizes and box sizes may be in %, px or em
+		init: function(){
+			this.load()
+			
+		},
 
-	##Table Task:
-	1. Your script must be able to handle number of columns dynamically (i.e. more or less columns, depending on the JSON sent)
-	- - - we are going to test the table with different JSONs during the test review
-	2. Get sample content with XMLHTTP request from here: http://cn.sbtech.com/sb-test/content.json
-	3. Decide on the table HTML structure by yourself (table/div... etc.)
-	4. Implement sorting on the column headers
+		load : function(){
+			var self = this,
+				data = null,
+				xhr = new XMLHttpRequest();
+			xhr.open('GET', '/js/MOCK_DATA.json');
+			xhr.onload = function() {
+			    if (xhr.status === 200) {
+			    	data = JSON.parse(xhr.responseText)
+			    	data.forEach(function(obj, i){
+			    		if(i < 1){
+			    			self.createTableElements.apply(self, [obj, 'th', true])
+			    		}
+			    	});
+			    	data.forEach(function(obj, i){
+			    		self.createTableElements.apply(self, [obj, 'td'])
+			    	});
+			    	self.blockTable.appendChild(self.table);
+			    	self.sortTable()	  
+			    }else{
+			       console.log('Request failed.  Returned status of ' + xhr.status);
+			    }
+			};
+			xhr.send();
+		},
 
-	##What are we checking about your code:
-	- structure
-	- quality
-	- consistency
-	- reusability
+		createTableElements : function(obj, cell, isTitle){
+			var	tr,
+				td,
+				th,
+				text,
+				dataText;
+			if(isTitle){
+				this.table.appendChild(document.createElement('thead'));
+			}else{
+				if(this.table.getElementsByTagName('tbody').length < 1){
+					this.table.appendChild(document.createElement('tbody'));
+				}
+			}
+			tr = document.createElement('tr');
+			for(key in obj){
+				td = document.createElement(cell)
+				if(isTitle){
+				  dataText = key;
+				}else{
+				  dataText = obj[key];
+				  switch(key.toLowerCase()) {
+  					case 'avatar'.toLowerCase():
 
-	##!!! IMPORTANT !!!
-	##Your test does NOT qualify for review if:
-	- JSON content is not loaded with AJAX
-	- We need to fiddle with your code to make it work
-	- JS global scope is polluted
-	- scripts you didn't write yourself
-	- JS libraries and frameworks
-	- CSS "frameworks"
-*/
+  						if(obj[key] !== null) {
+  							dataText = '<img src="'+obj[key]+'"/>'
+  						}else{
+  							dataText = '-'
+  						}
+  						break;
+  					case 'administrator'.toLowerCase():
+  						if(obj[key] == true){
+  							dataText = 'yes'
+  						}else{
+  							dataText = 'no'
+  						}
+  						break;
+  					};
+				}
+				td.innerHTML = dataText
+				this.table.lastChild.appendChild(tr).appendChild(td);
+			}
+		},
+
+		sortTable : function(){
+			var self = this;
+			var table = document.querySelector('#'+this.block+' table');
+		    table.onclick = function(e) {
+		      if (e.target.tagName != 'TH') return;
+		      sortTrTable(e.target.cellIndex, e);
+		    };
+		    function sortTrTable(colNum, e) {
+		      var tbody = table.getElementsByTagName('tbody')[0],
+		      	  rowsArray = [].slice.call(tbody.rows),
+		          compare = function (rowA, rowB) {
+		          	if(!isNaN(rowA.cells[colNum].innerHTML)){
+		          		return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+		          	}
+		          	if (rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML) {
+					    return 1;
+					}
+					if (rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML) {
+					    return -1;
+					}
+					return 0;
+		      };
+		      if(e.target.classList.contains('reverse')){
+		      	rowsArray.sort(compare).reverse();
+		      	e.target.classList.remove('reverse');
+		      }else{
+		      	rowsArray.sort(compare);
+		      	removeClases(table, e.target.tagName, 'reverse')
+		      	e.target.classList.add('reverse');
+		      }
+		      table.removeChild(tbody);
+		      for (var i = 0; i < rowsArray.length; i++) {
+		        tbody.appendChild(rowsArray[i]);
+		      }
+		      table.appendChild(tbody);
+		    }
+		},
+	}
+
+	function Accordion(param){
+		this.block = document.querySelector(param.block);
+		this.link = param.link;
+		this.classActive = param.classActive;
+	}
+
+	Accordion.prototype = {
+
+		constructor: Accordion,
+
+		init : function(){
+			this.open()
+		},
+
+		open : function(){
+			var self  = this,
+				targetParent,
+				setText = function(el){
+					el.children[0].innerHTML = 'Click to open';
+				}
+			this.block.addEventListener('click', function(e){
+				e.stopPropagation();
+				e.preventDefault();
+				targetParent = e.target.parentElement;
+				if(!e.target.classList.contains(self.link)) return;
+				if(targetParent.classList.contains(self.classActive)){
+			      	targetParent.classList.remove(self.classActive);
+			      	e.target.innerHTML = 'Click to open'
+			    }else{
+			      	removeClases(self.block, targetParent.tagName, self.classActive, setText)
+			      	targetParent.classList.add(self.classActive);
+			      	e.target.innerHTML = 'Click to close'
+			    }
+			});
+		}
+	}
+	
+	function Menu(param){
+		Accordion.apply(this, arguments);
+	}
+
+	Menu.prototype = {
+
+		constructor: Menu,
+
+		init : function(){
+			this.open()
+		},
+
+		open : function(){
+			var self = this;
+			this.block.addEventListener('mouseenter', function(e){
+			    this.classList.add(self.classActive);    
+			});
+			this.block.addEventListener("mouseleave", function () {
+			    this.classList.remove(self.classActive);
+			});
+		}
+	}
+	
+	function Flags(param){
+		Accordion.apply(this, arguments);
+	}
+
+	Flags.prototype = {
+
+		constructor: Menu,
+
+		init : function(){
+			this.open()
+		},
+
+		open : function(){
+			var self = this,
+				target,
+				targetParent;
+			this.block.addEventListener('click', function(e){
+				e.preventDefault();
+				target = e.target;
+				while (target != this) {
+				    if (target.tagName == 'LI') {
+				      targetParent = target
+				      break;
+				    }
+				    target = target.parentNode;
+				}
+				if(targetParent.classList.contains(self.classActive)){
+			      	targetParent.classList.remove(self.classActive);
+			    }else{
+			      	removeClases(self.block, targetParent.tagName, self.classActive)
+			      	targetParent.classList.add(self.classActive);
+			    }
+			})
+		}
+	}
+
+	var dataTable = new DataTable({
+		block: 'data-table'
+	});
+	dataTable.init();
+
+	var accordion = new Accordion({
+		block: '.accordion',
+		link: 'accordion__link',
+		classActive: 'open'
+	});
+	accordion.init();
+
+	var menu = new Menu({
+		block: '.main-menu__item.sub',
+		classActive : 'open'
+	});
+	menu.init();
+
+	var flags = new Flags({
+		block: '.flags',
+		link: 'flags__link',
+		classActive: 'flags__item_selected'
+	});
+	flags.init();
+
+}
+
+
